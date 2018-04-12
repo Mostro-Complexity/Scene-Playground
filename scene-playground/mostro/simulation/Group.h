363b4a6fad6271099043ea5bc662a8ea38edbb88
siyu qiu <bright196@163.com>
2018-04-12 23:36:37 +0800

    层次结构、目录树更改，进一步重构

---
+++ scene-playground/mostro/simulation/Group.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "stdafx.h"
+
+
+namespace mostro
+{
+	namespace simulation
+	{
+		enum CameraGroupType
+		{
+			roamCamera, observeCamera
+		};
+
+		class Group
+		{
+		public:
+
+			virtual void init() abstract;
+			virtual void render() abstract;
+
+		};
+	}
+}
